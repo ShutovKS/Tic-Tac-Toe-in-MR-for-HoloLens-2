9e35246998941149532cadbfd3bb75533b030931
ShutovKS <i@shutovks.ru>
2023-09-20 23:58:55 +0300

    Added main menu ui

---
+++ src/AR project for Hololens2/Assets/Scripts/UI/MainMenu/MainButtonsUI.cs
@@ -0,0 +1,12 @@
+﻿using MixedReality.Toolkit.UX;
+using UnityEngine;
+
+namespace UI.MainMenu
+{
+	public class MainButtonsUI : MonoBehaviour
+	{
+		[field: SerializeField] public PressableButton OnlineGameButton { get; private set; }
+		[field: SerializeField] public PressableButton OfflineGameButton { get; private set; }
+		[field: SerializeField] public PressableButton ExitButton { get; private set; }
+	}
+}
---
+++ src/AR project for Hololens2/Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace UI.MainMenu
+{
+	public class MainMenuUI : MonoBehaviour
+	{
+		[SerializeField] private MainButtonsUI _mainButtonsUI;
+		
+		public void RegisterOnlineGameButtonCallback(UnityAction callback)
+		{
+			_mainButtonsUI.OnlineGameButton.OnClicked.AddListener(callback);
+		}
+		
+		public void RegisterOfflineGameButtonCallback(UnityAction callback)
+		{
+			_mainButtonsUI.OfflineGameButton.OnClicked.AddListener(callback);
+		}
+		
+		public void RegisterExitButtonCallback(UnityAction callback)
+		{
+			_mainButtonsUI.ExitButton.OnClicked.AddListener(callback);
+		}
+	}
+}
