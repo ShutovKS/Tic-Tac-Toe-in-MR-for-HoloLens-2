26b06574ff4356db974d9e300531542fdf2cf307
ShutovKS <schutow.kir@yandex.ru>
2023-09-28 08:33:09 +0300

    Added instantiate input simulator for Unity Editor

--- src/AR project for Hololens2/Assets/Scripts/Infrastructure/Manager/Bootstrap/BootstrapManager.cs
+++ src/AR project for Hololens2/Assets/Scripts/Infrastructure/Manager/Bootstrap/BootstrapManager.cs
@@ -3,11 +3,26 @@ using UnityEngine.SceneManagement;
 
 namespace Infrastructure.Manager.Bootstrap
 {
-	public class BootstrapManager : MonoBehaviour
-	{
-		private void Start()
-		{
-			SceneManager.LoadScene("2.Meta");
-		}
-	}
+    public class BootstrapManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mrtkInputSimulator;
+        private void Start()
+        {
+#if UNITY_EDITOR
+            InitializedInputSimulator();
+#endif
+            SceneManager.LoadScene("2.Meta");
+        }
+
+        private void InitializedInputSimulator()
+        {
+            if (_mrtkInputSimulator == null)
+            {
+                Debug.LogError("MRTK Input Simulator is not set in BootstrapManager");
+                return;
+            }
+            var simulator = Instantiate(_mrtkInputSimulator);
+            DontDestroyOnLoad(simulator);
+        }
+    }
 }
