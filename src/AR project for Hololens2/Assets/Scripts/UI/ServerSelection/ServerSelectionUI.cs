126597b303b882387aea7ad175569a56f347bf08
ShutovKS <schutow.kir@yandex.ru>
2023-10-07 20:10:22 +0300

    Added server selection ui

---
+++ src/AR project for Hololens2/Assets/Scripts/UI/ServerSelection/ServerSelectionUI.cs
@@ -0,0 +1,24 @@
+﻿using MixedReality.Toolkit.UX;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UI.ServerSelection
+{
+    public class ServerSelectionUI : MonoBehaviour
+    {
+        [SerializeField] private PressableButton onHostButton;
+        [SerializeField] private PressableButton onClientButton;
+        [SerializeField] private PressableButton onExitButton;
+        [SerializeField] private GameObject serverSelectionPanel;
+        
+        public void RegisterHostButtonCallback(UnityAction callback) => onHostButton.OnClicked.AddListener(callback);
+        public void RegisterClientButtonCallback(UnityAction callback) => onClientButton.OnClicked.AddListener(callback);
+        public void RegisterExitButtonCallback(UnityAction callback) => onExitButton.OnClicked.AddListener(callback);
+        
+        public void UnregisterHostButtonCallback(UnityAction callback) => onHostButton.OnClicked.RemoveListener(callback);
+        public void UnregisterClientButtonCallback(UnityAction callback) => onClientButton.OnClicked.RemoveListener(callback);
+        public void UnregisterExitButtonCallback(UnityAction callback) => onExitButton.OnClicked.RemoveListener(callback);
+        
+        public void SetActivePanel(bool isActive) => serverSelectionPanel.SetActive(isActive);
+    }
+}
